f759dc2ea07ac4cf0fb87f5a0e557cef5060df56
aliyudaku <africamars@gmail.com>
2025-05-23 00:06:48 +0100

    streamlit test

---
+++ app3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("Surveillance Chat | Summary of Your CCTV Footage by AI")
+st.text("""Chat with your Surveillance Camera!""")
+st.caption("""Quickest way to review your security. Save hours of CCTV footage monitoring time using our AI solution.""")
