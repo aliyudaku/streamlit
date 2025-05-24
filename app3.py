590eb9e7bfc1c75ece2af1fa1399ed6983199a70
aliyudaku <africamars@gmail.com>
2025-05-25 00:55:07 +0100

    streamlit navbar

--- app3.py
+++ app3.py
@@ -1,4 +1,9 @@
 import streamlit as st
-st.title("Surveillance Chat | Summary of Your CCTV Footage by AI")
-st.text("""Chat with your Surveillance Camera!""")
+import pages as pg
+from streamlit_navigation_bar import st_navbar
+
+page = st_navbar(["Home", "Join Waiting List", "About Us", "Contact Us"])
+st.write(page)
+st.title("Summary of your CCTV Footage by AI")
+st.text("""Chat with your Security Camera!""")
 st.caption("""Quickest way to review your security. Save hours of CCTV footage monitoring time using our AI solution.""")
